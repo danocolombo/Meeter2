f35522c4ac5ac1adce3685216c12104bc2640d98
dano@dcolombo.com <GR0mans1212!>
2019-01-23 18:05:35 -0500

    recovery password progressing

--- auth/recoverPassword.php
+++ auth/recoverPassword.php
@@ -7,56 +7,50 @@
  */
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<title>Meeter Web Application</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="meeter.css" />
-<script src="https://apis.google.com/js/platform.js" async defer></script>
-<script src="js/jquery/jquery-3.3.1.js" type="text/javascript"></script>
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
-	$(document).ready(function(){
+	$(document).ready(function() {
 		$("#btnRecover").click(function(){
-		    alert("Email: " + $("#emailAddress").text());
-		  });
-//     	function recoverAccess(){
-    // 		var em = document.getElementById("EmailAddress").value();
-    //     	if( !validateEmail(em) {
-    //     		alert("email okay.");
-    //     	}else{
-    //     		alert("invalid email.");
-    //     	}
-//     		alert($("#emailAddress").text());	
-//         	return false;
-//     	}
-//         function validateEmail($email) {
-//     	  var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-//     	  return emailReg.test( $email );
-//     	}
-	}
-    </script>
+			var em = $("#emailAddress").val();
+			if (em.length < 8){
+				alert("You need to provide a well-formed email address.");
+				return false;
+			}
+			if(validateEmail(em)){
+				alert("good email");
+			}else{
+				alert("You need to provide a well-formed email address.");
+				return false;
+			}
+			return false;
+		});
+		function validateEmail($email) {
+			var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+			return emailReg.test( $email );
+		}
+	});
+</script>
 </head>
-<body onload="checkClearance()">
-	<div id="page">
-		<!-- [content] -->
-		<section id="content">
-			<form id="frmRecoverPassword" method="post">
-				<div class="recoverBox">
-					<fieldset style="width: 300px;">
-						<legend>&nbsp;Meeter Web Application&nbsp;</legend>
-						<div class="loginText">
-							<p>Enter the email you have on record.</p>
-							<p><input type="text" id="emailAddress" size='30'><br>
-							<p><br/>
-							<button type='button' id='btnRecover' style='background:green;color:white;'>&nbsp;RECOVER ACCESS&nbsp;</button>
-						</div>
-					</fieldset>
+<body>
+	<form id="frmRecoverPassword" method="post">
+		<div class="recoverBox">
+			<fieldset style="width: 300px;">
+				<legend>&nbsp;Meeter Web Application&nbsp;</legend>
+				<div class="loginText">
+					<p>
+					<p>Enter the email you have on record.</p>
+					<p>
+						<input type="text" id="emailAddress" size='40'><br>
+					</p>
+					<button type='button' id='btnRecover' style='background:green;color:white;'>&nbsp;RECOVER ACCESS&nbsp;</button>
 				</div>
-			</form>
-		</section>
-		<!-- [/content] -->
-	</div>
+			</fieldset>
+		</div>
+	</form>
 </body>
 </html>
+
 
