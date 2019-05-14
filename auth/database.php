7258b2164cc0adff611c2960a9c8095a7bb0aece
dano@dcolombo.com <GR0mans1212!>
2019-05-14 06:12:19 -0400

    relocating database file location

--- auth/database.php
+++ auth/database.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 global $connection;
 /*-----------------
  * database.php
@@ -7,14 +8,22 @@ global $connection;
  * http://www.developerdrive.com/2013/05/creating-a-simple-to-do-application-–-part-3/
  * 
  */
+$pword = "MR0mans1212!";
 if ( isset( $connection ) )
 	return;
 mysqli_report(MYSQLI_REPORT_STRICT);
 
+//define('DB_HOST', 'localhost');
+//define('DB_USER', 'dcolombo_muat');
+//define('DB_PASSWORD', 'MR0mans1212!');
+//define('DB_NAME', 'dcolombo_muat');
+
 define('DB_HOST', 'localhost');
-define('DB_USER', 'dcolombo_muat');
-define('DB_PASSWORD', 'MR0mans1212!');
-define('DB_NAME', 'dcolombo_muat');
+$tmp = "dcolombo_m" . $_SESSION['client'];
+define('DB_USER', $tmp);
+define('DB_PASSWORD', $pword);
+//$tmp = "dcolombo_m" && $_SESSION['client'];
+define('DB_NAME', $tmp);
 $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
