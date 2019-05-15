7376964a4de9889d03d942dcce5664c9a8b8ef20
dano@dcolombo.com <GR0mans1212!>
2019-05-14 21:15:02 -0400

    updated to lightsail database reference

--- auth/database.php
+++ auth/database.php
@@ -18,12 +18,13 @@ mysqli_report(MYSQLI_REPORT_STRICT);
 //define('DB_PASSWORD', 'MR0mans1212!');
 //define('DB_NAME', 'dcolombo_muat');
 
-define('DB_HOST', 'localhost');
-$tmp = "dcolombo_m" . $_SESSION['client'];
-define('DB_USER', $tmp);
-define('DB_PASSWORD', $pword);
+//define('DB_HOST', 'localhost');
+define('DB_HOST', 'ls-7ef76385fad42b2871f54789598686fa1e8ddf54.c2cwx5xdgogr.us-east-1.rds.amazonaws.com');
+//$tmp = "dcolombo_m" . $_SESSION['client'];
+define('DB_USER', 'dbmasteruser');
+define('DB_PASSWORD', 'S>gbnTsG+cvk:MlSz`W`H9>Nm_786Q.7');
 //$tmp = "dcolombo_m" && $_SESSION['client'];
-define('DB_NAME', $tmp);
+define('DB_NAME', 'rogueIntel1');
 $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
